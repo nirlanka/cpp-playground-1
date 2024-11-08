2b7db4903487b52a91ace24d894f462656f1c122
Nir Lanka <nir3al@gmail.com>
2024-11-08 21:55:46 +0800

    Add promise-cpp library manually

--- src/testproj02.cc
+++ src/testproj02.cc
@@ -7,6 +7,7 @@ int main(int argc, char **argv) {
       fmt::print("{} takes no arguments.\n", argv[0]);
         return 1;
     }
+    
     fmt::print("This is project {}.\n", ConstTexts::ProjectName);
     return 0;
 }
